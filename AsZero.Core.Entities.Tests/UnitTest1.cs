0c811e4a708f84027418835e64bc588674dc33e4
newbienewbie <itminus@hotmail.com>
2020-10-27 21:35:52 +0800

    Add two tests projects

---
+++ AsZero.Core.Entities.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace AsZero.Core.Entities.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
