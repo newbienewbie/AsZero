c4a29fd80e372a9f26f5a051f73bd39775b63c35
newbienewbie <itminus@hotmail.com>
2020-11-19 10:26:21 +0800

    code clean

--- AsZero.WebApi/Startup.cs
+++ AsZero.WebApi/Startup.cs
@@ -29,6 +29,9 @@ namespace AsZero.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddLogging(logging => {
+                logging.AddLog4Net();
+            });
             services.AddDbContext<AsZeroDbContext>(opts => {
                 opts.UseMySql(Configuration.GetConnectionString("AsZeroDbContext"));
             });
@@ -41,9 +44,6 @@ namespace AsZero.WebApi
                 .AddNewtonsoftJsonProtocol(opts => {
                     opts.PayloadSerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
                 });
-            services.AddLogging(logging => {
-                logging.AddLog4Net();
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
