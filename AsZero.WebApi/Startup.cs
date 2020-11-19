5bdcea0612dfb0d523888658ca9459dff9955b7f
newbienewbie <itminus@hotmail.com>
2020-11-19 10:50:38 +0800

    code clean

--- AsZero.WebApi/Startup.cs
+++ AsZero.WebApi/Startup.cs
@@ -30,6 +30,7 @@ namespace AsZero.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            #region infra
             services.AddLogging(logging => {
                 logging.AddLog4Net();
             });
@@ -40,6 +41,9 @@ namespace AsZero.WebApi
                 });
             });
             services.AddAsZeroHostedServices();
+            #endregion
+
+            #region web
             services.AddControllersWithViews()
                 .AddNewtonsoftJson(opts =>{
                     opts.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
@@ -48,13 +52,13 @@ namespace AsZero.WebApi
                 .AddNewtonsoftJsonProtocol(opts => {
                     opts.PayloadSerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
                 });
-
             // Forwarded headers
             services.Configure<ForwardedHeadersOptions>(opts => {
                 opts.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto | ForwardedHeaders.XForwardedHost;
                 opts.KnownNetworks.Clear();
                 opts.KnownProxies.Clear();
             });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
